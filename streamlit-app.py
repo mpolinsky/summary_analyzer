baad548772706051f2b62c7f790fb6b195b09f66
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-04 22:39:55 -0400

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -3,7 +3,7 @@ import streamlit as st
 l,c,r = st.columns([1,2,1])
 
 c.title("Text Analyzer..")
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns([1.5, 1, 1.5])
 
 col1.subheader("Article")
 col3.subheader("Summary")
