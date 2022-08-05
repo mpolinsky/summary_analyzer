d025a06e44b18c0128e5bb37eb2fb33e926f667b
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-05 08:11:55 -0400

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -10,9 +10,10 @@ def get_article_and_summary_n_grams(art, sum, n=2):
   return get_n_grams(art, n), get_n_grams(sum, n)
 
 # Returns list of n-grams in both, with value of n first.
+# I only need the ngrams from the summary that are also in the source.
 def compare_n_grams(art, sum, N=2):
   A, B = get_article_and_summary_n_grams(art, sum, n=N)
-  return [i for i in A if i in B] if len(A) > len(B) else [i for i in B if i in A]
+  return [i for i in B if i in A]    ##  previous start of expression.  
 
 l,c,r = st.columns([1,2,1])
 
@@ -22,6 +23,7 @@ col1, col2, col3 = st.columns([1.5, 1, 1.5])
 col1.subheader("Article")
 col3.subheader("Summary")
 
+# Right now I am getting the number of n-grams in the summary that are also in the source.
 with st.form("Entry"):
   article = col1.text_input("Enter article: ")
   summary = col3.text_input("Enter summary: ")
@@ -32,12 +34,10 @@ with st.form("Entry"):
     st.subheader(f"{len(common_n_grams)} found.")
     if len(common_n_grams) > 0:
       st.write(common_n_grams)
-    #col1.write(article)
-    #col3.write(summary)
 
 st.button("Reset")
     
-st.subheader("End")
+
 
 
 
