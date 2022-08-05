bfb2080a8073e2fecd1929021f948dad71e285e5
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-05 06:31:20 -0400

    Added input widgets

--- streamlit-app.py
+++ streamlit-app.py
@@ -7,3 +7,15 @@ col1, col2, col3 = st.columns([1.5, 1, 1.5])
 
 col1.subheader("Article")
 col3.subheader("Summary")
+
+with st.form("Entry"):
+  article = col1.text_input("Enter article: ")
+  summary = col2.text_input("Enter summary: ")
+
+col1.write(article)
+col2.write(summary)
+
+st.subheader("End")
+
+
+
