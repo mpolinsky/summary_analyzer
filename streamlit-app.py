f2e7a62c82968af6db6d050e6b068d2438e5764a
m. polinsky <30514239+mpolinsky@users.noreply.github.com>
2022-08-05 07:14:04 -0400

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -16,7 +16,7 @@ col3.subheader("Summary")
 with st.form("Entry"):
   article = col1.text_input("Enter article: ")
   summary = col3.text_input("Enter summary: ")
-  N = st.slider("Pick a value for n-grams", 0, len(summary.split(" . "))-1 )
+  N = st.slider("Pick a value for n-grams", 0, len(summary.split(". "))-1 )
   submit = st.form_submit_button("Select")
   if submit:
     compare_n_grams(article, summary, n=N)
